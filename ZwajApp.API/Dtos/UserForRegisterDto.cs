742ea521146b1aa7f9e1080303d596c0d33b2e14
= <=>
2019-03-15 04:10:30 +0300

    end of section 3

---
+++ ZwajApp.API/Dtos/UserForRegisterDto.cs
@@ -0,0 +1,13 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace ZwajApp.API.Dtos
+{
+    public class UserForRegisterDto
+    {
+        [Required]
+        public string Username { get; set; }
+
+        [StringLength(10,MinimumLength=6,ErrorMessage="يجب ان تكون كلمة المرور اقل من 10 واكبر 6")]
+        public string Password { get; set; }
+    }
+}
